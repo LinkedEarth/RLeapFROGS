a74523ad07afd288339f1dcab4ab62ab91e8307b
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-05-16 13:22:53 -0700

    Create exc_02_04.R

---
+++ exercises/exc_02_04.R
@@ -0,0 +1,11 @@
+substring("hello",1,3)
+text1 <- "hello"
+text1 <- substring(text1,1,3)
+text1
+
+firstLetter <- function(string){
+  string <- substring(string,1,1)
+  return(string)
+}
+
+firstLetter("alphabet")
