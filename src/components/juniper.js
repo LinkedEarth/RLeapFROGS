dad14b3b0af68f44fb26abc9d7539ccaad343ee4
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-05-16 09:07:35 -0700

    Update juniper.js

--- src/components/juniper.js
+++ src/components/juniper.js
@@ -110,7 +110,7 @@ class Juniper extends React.Component {
      * @returns {Promise} - Resolved with Binder settings, rejected with Error.
      */
     requestBinder(repo, branch, url) {
-        const binderUrl = `${url}/build/gh/${repo}/${branch}`
+        const binderUrl = `${url}/build/gh/${repo}/${branch}/main`
         this.log(() => console.info('building', { binderUrl }))
         return new Promise((resolve, reject) => {
             const es = new EventSource(binderUrl)
