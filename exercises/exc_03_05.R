8bccb3140ccf8ce1b8f3d5ac908bef9a35b5588b
Dave Edge <112658177+DaveEdge1@users.noreply.github.com>
2024-05-21 14:19:45 -0700

    Update exc_03_05.R

--- exercises/exc_03_05.R
+++ exercises/exc_03_05.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
+iris <- tibble(iris)
+
+iris
+
 iris.Sepal <- iris %>%
   select(Sepal.Length, Sepal.Width, Species) %>%
   nest(Sepal.Lengths = Sepal.Length, Sepal.Widths = Sepal.Width)
